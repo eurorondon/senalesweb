2ad67f7cf67e464149b3d14342fd301a04a5d046
eurorondon03 <eurorondon03@gmail.com>
2022-03-17 15:52:15 -0400

    v2_senalesweb

--- src/App.svelte
+++ src/App.svelte
@@ -2,98 +2,244 @@
 </script>
 
 <main>
-	<div class="container bg-danger">
-		<h1>Abierta</h1>
-		<div
-			id="carouselExampleControls"
-			class="carousel slide"
-			data-bs-ride="carousel"
-		>
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img
-						src="img/0591.jpg"
-						class="img-fluid d-block w-100"
-						alt="..."
-					/>
-				</div>
-				<div class="carousel-item">
-					<img src="img/0592.jpg" class="d-block w-100" alt="..." />
+	<div class="container">
+		<h1 class="text-center">Trading Latino SPOT</h1>
+		<div class="accordion" id="accordionExample">
+			<div class="accordion-item">
+				<h2 class="accordion-header" id="headingOne">
+					<button
+						class="accordion-button"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseOne"
+						aria-expanded="true"
+						aria-controls="collapseOne"
+					>
+						Trading Latino / NEAR SEMANAL 17-03-22
+					</button>
+				</h2>
+				<div
+					id="collapseOne"
+					class="accordion-collapse collapse show"
+					aria-labelledby="headingOne"
+					data-bs-parent="#accordionExample"
+				>
+					<div class="accordion-body">
+						<h1>Abierta</h1>
+						<div
+							id="carouselExampleControls"
+							class="carousel slide"
+							data-bs-ride="carousel"
+						>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img
+										src="img/0591.jpg"
+										class="img-fluid d-block w-100"
+										alt="..."
+									/>
+								</div>
+								<div class="carousel-item">
+									<img
+										src="img/0592.jpg"
+										class="d-block w-100"
+										alt="..."
+									/>
+								</div>
+							</div>
+							<button
+								class="carousel-control-prev"
+								type="button"
+								data-bs-target="#carouselExampleControls"
+								data-bs-slide="prev"
+							>
+								<span
+									class="carousel-control-prev-icon"
+									aria-hidden="true"
+								/>
+								<span class="visually-hidden">Previous</span>
+							</button>
+							<button
+								class="carousel-control-next"
+								type="button"
+								data-bs-target="#carouselExampleControls"
+								data-bs-slide="next"
+							>
+								<span
+									class="carousel-control-next-icon"
+									aria-hidden="true"
+								/>
+								<span class="visually-hidden">Next</span>
+							</button>
+						</div>
+						<h5>
+							📍ID DE SEÑAL: #0607📍 MONEDA: $DOT/USDT (3-5x)
+							Dirección: LARGA📈 <br />
+							➖➖➖➖➖➖➖ Bottom confirmado después de más de un
+							par de sólidas repeticiones de alto volumen. Rumbo a
+							nuestros máximos a corto y mediano plazo 😘 ENTRADA:
+							16.80 - 18.90 OTA: 17.74 OBJETIVOS Corto Plazo: 19.20
+							- 19.60 - 20.20 - 21 - 22 - 23.50 Medio Plazo: 25.50
+							- 28 - 32 - 37 - 42 - 49 STOP LOSS: 15.19 ➖➖➖➖➖➖➖
+							Este mensaje no se puede reenviar ni replicar - Binance
+							Killers®
+						</h5>
+					</div>
 				</div>
 			</div>
-			<button
-				class="carousel-control-prev"
-				type="button"
-				data-bs-target="#carouselExampleControls"
-				data-bs-slide="prev"
-			>
-				<span class="carousel-control-prev-icon" aria-hidden="true" />
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button
-				class="carousel-control-next"
-				type="button"
-				data-bs-target="#carouselExampleControls"
-				data-bs-slide="next"
-			>
-				<span class="carousel-control-next-icon" aria-hidden="true" />
-				<span class="visually-hidden">Next</span>
-			</button>
-		</div>
-		<h5>
-			📍ID DE SEÑAL: #0607📍 MONEDA: $DOT/USDT (3-5x) Dirección: LARGA📈 <br
-			/>
-			➖➖➖➖➖➖➖ Bottom confirmado después de más de un par de sólidas
-			repeticiones de alto volumen. Rumbo a nuestros máximos a corto y mediano
-			plazo 😘 ENTRADA: 16.80 - 18.90 OTA: 17.74 OBJETIVOS Corto Plazo: 19.20
-			- 19.60 - 20.20 - 21 - 22 - 23.50 Medio Plazo: 25.50 - 28 - 32 - 37 -
-			42 - 49 STOP LOSS: 15.19 ➖➖➖➖➖➖➖ Este mensaje no se puede reenviar
-			ni replicar - Binance Killers®
-		</h5>
-	</div>
-	<div class="container bg-primary">
-		<div
-			id="carouselExampleControls1"
-			class="carousel slide"
-			data-bs-ride="carousel"
-		>
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img
-						src="img/0591.jpg"
-						class="img-fluid d-block w-100"
-						alt="..."
-					/>
-				</div>
-				<div class="carousel-item">
-					<img src="img/0592.jpg" class="d-block w-100" alt="..." />
+			<div class="accordion-item">
+				<h2 class="accordion-header" id="headingTwo">
+					<button
+						class="accordion-button collapsed"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseTwo"
+						aria-expanded="false"
+						aria-controls="collapseTwo"
+					>
+						Trading Latino / AVAX SEMANAL 17-03-22
+					</button>
+				</h2>
+				<div
+					id="collapseTwo"
+					class="accordion-collapse collapse"
+					aria-labelledby="headingTwo"
+					data-bs-parent="#accordionExample"
+				>
+					<div class="accordion-body">
+						<div
+							id="carouselExampleControls1"
+							class="carousel slide"
+							data-bs-ride="carousel"
+						>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img
+										src="img/0591.jpg"
+										class="img-fluid d-block w-100"
+										alt="..."
+									/>
+								</div>
+								<div class="carousel-item">
+									<img
+										src="img/0592.jpg"
+										class="d-block w-100"
+										alt="..."
+									/>
+								</div>
+							</div>
+							<button
+								class="carousel-control-prev"
+								type="button"
+								data-bs-target="#carouselExampleControls1"
+								data-bs-slide="prev"
+							>
+								<span
+									class="carousel-control-prev-icon"
+									aria-hidden="true"
+								/>
+								<span class="visually-hidden">Previous</span>
+							</button>
+							<button
+								class="carousel-control-next"
+								type="button"
+								data-bs-target="#carouselExampleControls1"
+								data-bs-slide="next"
+							>
+								<span
+									class="carousel-control-next-icon"
+									aria-hidden="true"
+								/>
+								<span class="visually-hidden">Next</span>
+							</button>
+						</div>
+						<h1>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Maxime tempora sapiente ea fugiat dignissimos
+							commodi perferendis possimus excepturi? Assumenda,
+							aspernatur.
+						</h1>
+					</div>
 				</div>
 			</div>
-			<button
-				class="carousel-control-prev"
-				type="button"
-				data-bs-target="#carouselExampleControls1"
-				data-bs-slide="prev"
-			>
-				<span class="carousel-control-prev-icon" aria-hidden="true" />
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button
-				class="carousel-control-next"
-				type="button"
-				data-bs-target="#carouselExampleControls1"
-				data-bs-slide="next"
-			>
-				<span class="carousel-control-next-icon" aria-hidden="true" />
-				<span class="visually-hidden">Next</span>
-			</button>
+			<div class="accordion-item">
+				<h2 class="accordion-header" id="headingThree">
+					<button
+						class="accordion-button collapsed"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#collapseThree"
+						aria-expanded="false"
+						aria-controls="collapseThree"
+					>
+						Trading Latino / EOS SEMANAL 17-03-22
+					</button>
+				</h2>
+				<div
+					id="collapseThree"
+					class="accordion-collapse collapse"
+					aria-labelledby="headingThree"
+					data-bs-parent="#accordionExample"
+				>
+					<div class="accordion-body">
+						<div
+							id="carouselExampleControls1"
+							class="carousel slide"
+							data-bs-ride="carousel"
+						>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img
+										src="img/0591.jpg"
+										class="img-fluid d-block w-100"
+										alt="..."
+									/>
+								</div>
+								<div class="carousel-item">
+									<img
+										src="img/0592.jpg"
+										class="d-block w-100"
+										alt="..."
+									/>
+								</div>
+							</div>
+							<button
+								class="carousel-control-prev"
+								type="button"
+								data-bs-target="#carouselExampleControls1"
+								data-bs-slide="prev"
+							>
+								<span
+									class="carousel-control-prev-icon"
+									aria-hidden="true"
+								/>
+								<span class="visually-hidden">Previous</span>
+							</button>
+							<button
+								class="carousel-control-next"
+								type="button"
+								data-bs-target="#carouselExampleControls1"
+								data-bs-slide="next"
+							>
+								<span
+									class="carousel-control-next-icon"
+									aria-hidden="true"
+								/>
+								<span class="visually-hidden">Next</span>
+							</button>
+						</div>
+						<h1>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Maxime tempora sapiente ea fugiat dignissimos
+							commodi perferendis possimus excepturi? Assumenda,
+							aspernatur.
+						</h1>
+					</div>
+				</div>
+			</div>
 		</div>
-		<h1>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-			tempora sapiente ea fugiat dignissimos commodi perferendis possimus
-			excepturi? Assumenda, aspernatur.
-		</h1>
 	</div>
+	<div class="container bg-primary" />
 </main>
 
 <style>
