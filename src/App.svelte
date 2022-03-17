d479679043359f2e054ce1e5013f0707e86e535b
eurorondon03 <eurorondon03@gmail.com>
2022-03-17 15:20:05 -0400

    v1_senalesweb

---
+++ src/App.svelte
@@ -0,0 +1,100 @@
+<script>
+</script>
+
+<main>
+	<div class="container bg-danger">
+		<h1>Abierta</h1>
+		<div
+			id="carouselExampleControls"
+			class="carousel slide"
+			data-bs-ride="carousel"
+		>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img
+						src="img/0591.jpg"
+						class="img-fluid d-block w-100"
+						alt="..."
+					/>
+				</div>
+				<div class="carousel-item">
+					<img src="img/0592.jpg" class="d-block w-100" alt="..." />
+				</div>
+			</div>
+			<button
+				class="carousel-control-prev"
+				type="button"
+				data-bs-target="#carouselExampleControls"
+				data-bs-slide="prev"
+			>
+				<span class="carousel-control-prev-icon" aria-hidden="true" />
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button
+				class="carousel-control-next"
+				type="button"
+				data-bs-target="#carouselExampleControls"
+				data-bs-slide="next"
+			>
+				<span class="carousel-control-next-icon" aria-hidden="true" />
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+		<h5>
+			📍ID DE SEÑAL: #0607📍 MONEDA: $DOT/USDT (3-5x) Dirección: LARGA📈 <br
+			/>
+			➖➖➖➖➖➖➖ Bottom confirmado después de más de un par de sólidas
+			repeticiones de alto volumen. Rumbo a nuestros máximos a corto y mediano
+			plazo 😘 ENTRADA: 16.80 - 18.90 OTA: 17.74 OBJETIVOS Corto Plazo: 19.20
+			- 19.60 - 20.20 - 21 - 22 - 23.50 Medio Plazo: 25.50 - 28 - 32 - 37 -
+			42 - 49 STOP LOSS: 15.19 ➖➖➖➖➖➖➖ Este mensaje no se puede reenviar
+			ni replicar - Binance Killers®
+		</h5>
+	</div>
+	<div class="container bg-primary">
+		<div
+			id="carouselExampleControls1"
+			class="carousel slide"
+			data-bs-ride="carousel"
+		>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img
+						src="img/0591.jpg"
+						class="img-fluid d-block w-100"
+						alt="..."
+					/>
+				</div>
+				<div class="carousel-item">
+					<img src="img/0592.jpg" class="d-block w-100" alt="..." />
+				</div>
+			</div>
+			<button
+				class="carousel-control-prev"
+				type="button"
+				data-bs-target="#carouselExampleControls1"
+				data-bs-slide="prev"
+			>
+				<span class="carousel-control-prev-icon" aria-hidden="true" />
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button
+				class="carousel-control-next"
+				type="button"
+				data-bs-target="#carouselExampleControls1"
+				data-bs-slide="next"
+			>
+				<span class="carousel-control-next-icon" aria-hidden="true" />
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+		<h1>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+			tempora sapiente ea fugiat dignissimos commodi perferendis possimus
+			excepturi? Assumenda, aspernatur.
+		</h1>
+	</div>
+</main>
+
+<style>
+</style>
